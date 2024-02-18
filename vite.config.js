22e603528c57a23e2e0fa11f28fd7f889851afc8
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-18 20:40:23 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()], //add eslint later
-  //plugins: [react(),eslint()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.supabaseKey': JSON.stringify(env.supabaseKey)
+    },
+    plugins: [react()],
+  }
 })
