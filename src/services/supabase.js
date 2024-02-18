d935178ceb2f28166fbac17853f8d937cd5410bc
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-18 20:33:11 +0530

    hide the supabaseKey

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ipmcodordginsmosxniv.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwbWNvZG9yZGdpbnNtb3N4bml2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyNTQ1ODQsImV4cCI6MjAyMzgzMDU4NH0.WFSdMoPMiO1ftQA3TqUwAD-T9AZ9AztC1aTqfvcJfhU"
+const supabaseKey = process.env.supabaseKey;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
