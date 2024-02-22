13355095ec79ee3d1886ec1d22cb0149232f785d
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-22 12:21:06 +0530

    aaded reusable Menu model

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 
 
@@ -30,7 +31,8 @@ const CabinTable = () => {
   }
 
   return (
-    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+    <Menus>
+        <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
         <div></div>
         <div>Cabin</div>
@@ -44,6 +46,8 @@ const CabinTable = () => {
       )}/>
       
     </Table>
+    </Menus>
+  
   );
 };
 
--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -1,6 +1,10 @@
+import { createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
+import { HiEllipsisVertical } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
-const StyledMenu = styled.div`
+const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -60,3 +64,70 @@ const StyledButton = styled.button`
     transition: all 0.3s;
   }
 `;
+
+const MenusContex = createContext();
+
+const Menus = ({ children }) => {
+  const [openId, setOpenId] = useState("");
+  const [position, setPosition] = useState(null);
+  const close = () => setOpenId("");
+  const open = setOpenId;
+
+  return (
+    <MenusContex.Provider value={{ openId, close, open,position, setPosition}}>
+      {children}
+    </MenusContex.Provider>
+  );
+};
+
+function Toggle({ id }) {
+  const { openId, close, open,setPosition } = useContext(MenusContex);
+  function handelClick(e) {
+    const rect = e.target.closest('button').getBoundingClientRect(); 
+    setPosition({
+      x:window.innerWidth -rect.width - rect.x,
+      y:rect.y+rect.height+8,
+    })
+
+    console.log("value of rect ==> "+rect);
+
+    openId === "" || openId !== id ? open(id) : close();
+
+  }
+  return (
+    <StyledToggle onClick={handelClick}>
+      <HiEllipsisVertical />
+    </StyledToggle>
+  );
+}
+function List({ id, children }) {
+  const { openId,close,position } = useContext(MenusContex);
+const ref = useOutsideClick(close);
+  if (openId !== id) return null;
+
+  return createPortal(
+    <StyledList position={position} ref={ref}>{children}</StyledList>,
+    document.body
+  );
+}
+
+function Button({ children,icon,onClick}) {
+  const {close} = useContext(MenusContex);
+  function handelClick()
+  {
+    onClick?.();
+    close();
+  }
+  return (
+    <li>
+      <StyledButton onClick={handelClick}>{icon}<span>{children}</span></StyledButton>
+    </li>
+  );
+}
+
+Menus.Menu = Menu;
+Menus.Toggle = Toggle;
+Menus.List = List;
+Menus.Button = Button;
+
+export default Menus;
