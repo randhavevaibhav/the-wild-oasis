a185e7df62d17f5c28173bb33b9a99c84c904a83
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-24 23:40:09 +0530

    removed temp pass

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,7 +8,7 @@ import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("test1@gmail.com");
-  const [password, setPassword] = useState("123");
+  const [password, setPassword] = useState("");
   const{login,isLoading} = useLogin();
 
   function handleSubmit(e) {
