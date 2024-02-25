b54cfcf7bf23a46369dfb23125b6d27ea26bb0fe
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-25 20:27:08 +0530

    added user avatar in heading

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
-import LogOut from "../features/authentication/LogOut";
-
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar"
 const StyledHeader = styled.header`
 
 background-color: var(--color-grey-0);
 padding: 1.2rem 4.8rem;
 border-bottom: 1px solid var(--color-grey-100);
+display: flex;
+gap: 2.4rem;
+align-items: center;
+justify-content: flex-end;
 
 
 `;
@@ -13,7 +17,9 @@ border-bottom: 1px solid var(--color-grey-100);
 const Header = () => {
   return (
     <StyledHeader>
-        <LogOut/>
+      <UserAvatar/>
+        <HeaderMenu/>
+        
     </StyledHeader>
   )
 }
