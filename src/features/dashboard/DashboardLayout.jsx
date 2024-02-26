a038e5328b6019f424562c9f68ac52018869e11a
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-26 14:33:18 +0530

    fetched booking and stays for dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,31 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+
+
+const DashboardLayout = () => {
+  const {bookings,isLoading1} = useRecentBookings();
+  const{stays,confirmedStays,isLoading:isLoading2} =useRecentStays();
+  if(isLoading1 ||isLoading2) return<Spinner/>
+
+  //console.log("Bookings in D===> "+JSON.stringify(bookings))
+  return (
+    <StyledDashboardLayout>
+      <div>
+      Statistics
+      </div>
+      <div>
+      Todays activities
+      </div>
+      <div>
+      Chart stay duration
+      </div>
+      <div>
+      Chart Sales
+      </div>
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
