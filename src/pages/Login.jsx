2a993c233fa44c34b601aa8cefc97157b376ec6d
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-28 11:49:12 +0530

    added responsiveness to login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,33 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
 import Heading from "../ui/Heading";
 import Logo from "../ui/Logo";
+import { useShowHideSidebar } from "../context/showHideSideBarContex";
+
+const displayOptions = {
+  mobile:css`grid-template-columns: 35rem;`,
+  desktop:css`grid-template-columns: 35rem;`
+}
 const LoginLayout = styled.main`
   min-height: 100vh;
   display: grid;
-  grid-template-columns: 48rem;
+  ${props=>displayOptions[props.mode]};
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
   background-color: var(--color-grey-50);
 `;
 
+LoginLayout.defaultProps = {
+  
+  mode:"desktop"
+}
+
+
 function Login() {
+  const {mode} =useShowHideSidebar();
   return (
-    <LoginLayout>
+    <LoginLayout mode={mode}>
       <Logo/>
       <Heading as="h4">Log in to your account</Heading>
       <LoginForm />
