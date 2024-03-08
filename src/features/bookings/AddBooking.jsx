93d22964c260fd50e3b82a0ff80cc3592548506d
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-03-08 15:56:09 +0530

    added validation if all guests are booked

--- src/features/bookings/AddBooking.jsx
+++ src/features/bookings/AddBooking.jsx
@@ -1,22 +1,35 @@
 import Button from "../../ui/Button";
 import CreateBookingForm from "../bookings/CreateBookingForm";
 import Modal from "../../ui/Modal";
+import { useGuestsWithNoBooking } from "../Guests/useGuestsWithNoBooking";
 
+import SpinnerMini from "../../ui/SpinnerMini"
 
 const AddBooking = () => {
-  return (
-    <div>
+  const {guests,isLoading} = useGuestsWithNoBooking();
+
+  if(isLoading) return <SpinnerMini/>
+
+  const guestsLength = guests?.length;
+  
+  return (<>
+  {guestsLength>0?<div>
       <Modal>
         <Modal.Open opens="booking-form">
-          <Button>Add new Booking</Button>
+          <Button >Add new Booking</Button>
         </Modal.Open>
         <Modal.Window name="booking-form">
           <CreateBookingForm />
         </Modal.Window>
       </Modal>
-    </div>
+    </div>:<div>All Guests are booked !!</div>}
+    
+    
+    
+    </>
+    
   );
-};
+}
 
 
 
